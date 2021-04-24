776c8b6539aa63071587d4d2b3fb7d10329cc403
jain-arham-hsr <jain.arham.hsr@gmail.com>
2021-04-24 21:26:50 +0530

    Initial Commit

---
+++ dev/test.js
@@ -0,0 +1,13 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+// bitcoin.createNewBlock(2389, "5VM3840958M40958", "MVIJGM89056009");
+
+// bitcoin.createNewTransaction(100, 'ALEXU3U54UJG03', 'JENNM5U45M45UJHE');
+
+// bitcoin.createNewBlock(2389, "M549M58439M53490", "4958M945834584");
+
+// bitcoin.createNewBlock(2389, "ETU98MU8U594MTU5", "KDF5JGM5945455");
+
+console.log(bitcoin);
